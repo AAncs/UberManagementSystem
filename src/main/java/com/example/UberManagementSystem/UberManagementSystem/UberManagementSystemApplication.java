038a183932e116bcac765222a462392292f1ffae
aanjain <jainaanchal2620@gmail.com>
2023-03-17 14:57:51 +0530

    Initial commit

---
+++ src/main/java/com/example/UberManagementSystem/UberManagementSystem/UberManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.UberManagementSystem.UberManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UberManagementSystem/UberManagementSystem/UberManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UberManagementSystem.UberManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
